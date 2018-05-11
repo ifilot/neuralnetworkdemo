a501ecc522a7099d2ac7cf54a2b872fc50d2e8f4
ifilot <ivo@zuidstijl.nl>
2018-05-11 15:24:16 +0200

    Implementing TCLAP and multiple execution modes

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # neuralnetworkdemo
 Neural Network Demo
 
+![4.png](tests/4.png) ![2.png](tests/2.png)
+
 # Compilation
 ```
 mkdir build
 cd build
 cmake ../src
 make -j9 && make test
+```
+
+# Usage
+
+To train the network (using previously trained network)
+```
+./neuralnetworkdemo -t -i ../tests/image.ann -o ../tests/image.ann
+```
+
+To train the network from scratch
+```
+./neuralnetworkdemo -t -o ../tests/image.ann
+```
+
+To use the trained network to classify an image (i.e. recognize the hand-writing)
+```
+./neuralnetworkdemo -f ../tests/2.png -i ../tests/image.ann
 ```
--- src/neuralnetworkdemo.cpp
+++ src/neuralnetworkdemo.cpp
@@ -29,36 +29,125 @@
 #include "config.h"
 #include "neural_network.h"
 #include "mnist_loader.h"
+#include "pngfuncs.h"
 
+#include <memory>
 #include <iostream>
 #include <omp.h>
 #include <chrono>
 #include <boost/format.hpp>
+#include <tclap/CmdLine.h>
 
 int main(int argc, char* argv[]) {
 
     omp_set_num_threads(1);
 
-    auto start = std::chrono::system_clock::now();
+    try {
 
-    MNISTLoader ml;
-    ml.load_testset("../data/t10k-images-idx3-ubyte.gz", "../data/t10k-labels-idx1-ubyte.gz");
-    ml.load_trainingset("../data/train-images-idx3-ubyte.gz", "../data/train-labels-idx1-ubyte.gz");
+        TCLAP::CmdLine cmd("neuralnetworkdemo", ' ', VERSION);
 
-    auto trainingset = ml.get_trainingset();
-    auto testset = ml.get_testset();
+        // input file
+        TCLAP::ValueArg<std::string> arg_input("i","input","Input file",false,"","filename");
+        cmd.add(arg_input);
 
-    //NeuralNetwork nn({784,30,10});
-    NeuralNetwork nn("test.net");
+        // density file
+        TCLAP::ValueArg<std::string> arg_output("o","output","Output file",false,"","filename");
+        cmd.add(arg_output);
 
-    nn.sgd(trainingset, testset, 10, 10, 3.0);
+        // density file
+        TCLAP::ValueArg<std::string> arg_image("f","image","Image file",false,"","filename");
+        cmd.add(arg_image);
 
-    nn.save_network("test.net");
+        // lua file
+        TCLAP::SwitchArg arg_train("t","train","whether to further train network");
+        cmd.add(arg_train);
 
-    auto end = std::chrono::system_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << boost::format("Total elapsed time: %f ms\n") % elapsed.count();
-    std::cout << "--------------------------------------------------------------" << std::endl;
+        cmd.parse(argc, argv);
+
+        bool train = arg_train.getValue();
+        const std::string input_filename = arg_input.getValue();
+        const std::string output_filename = arg_output.getValue();
+        const std::string image_filename = arg_image.getValue();
+
+        if(train) {
+            auto start = std::chrono::system_clock::now();
+
+            if(output_filename.empty()) {
+                throw std::runtime_error("You need to specify an output file");
+            }
+
+            MNISTLoader ml;
+            ml.load_testset("../data/t10k-images-idx3-ubyte.gz", "../data/t10k-labels-idx1-ubyte.gz");
+            ml.load_trainingset("../data/train-images-idx3-ubyte.gz", "../data/train-labels-idx1-ubyte.gz");
+
+            auto trainingset = ml.get_trainingset();
+            auto testset = ml.get_testset();
+
+            std::unique_ptr<NeuralNetwork> nn;
+
+            if(input_filename.empty()) {
+                nn = std::make_unique<NeuralNetwork>(std::vector<uint32_t>({784,30,10}));
+            } else {
+                std::cout << "Loading network from: " << input_filename << std::endl;
+                nn = std::make_unique<NeuralNetwork>(input_filename);
+            }
+
+            nn->sgd(trainingset, testset, 10, 10, 3.0);
+
+            std::cout << "Writing to " << output_filename << std::endl;
+            nn->save_network(output_filename);
+
+            auto end = std::chrono::system_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+            std::cout << boost::format("Total elapsed time: %f ms\n") % elapsed.count();
+            std::cout << "--------------------------------------------------------------" << std::endl;
+        } else {
+            /*
+             * Read sample image
+             */
+            if(input_filename.empty()) {
+                throw std::runtime_error("You need to specify an input file for the network");
+            }
+
+            // load neural network from file
+            NeuralNetwork nn(input_filename);
+
+            // grab image
+            std::cout << "Reading " << image_filename << std::endl;
+            std::vector<uint8_t> buffer;
+            png_uint_32 width, height;
+            int col, bit_depth;
+            PNG::load_image_buffer_from_png(image_filename, buffer, &width, &height, &col, &bit_depth);
+
+            if(width != 28 || height != 28) {
+                throw std::runtime_error("Image needs to be 28x28 px!");
+            }
+
+            if(col != PNG_COLOR_TYPE_GRAY) {
+                throw std::runtime_error("Image needs to be saved in grayscale with no alpha channel!");
+            }
+
+            // conver to input structure
+            std::vector<double> in;
+            for(unsigned int i=0; i<buffer.size(); i++) {
+                in.push_back((double)buffer[i] / 255.0);
+            }
+
+            // perform feed forward and output result
+            nn.feed_forward(in);
+            auto v = nn.get_output();
+            std::cout << "--------------------------------------------------------------" << std::endl;
+            std::cout << "This image is classified as \"";
+            std::cout << std::distance(v.begin(), std::max_element(v.begin(), v.end()));
+            std::cout << "\"" << std::endl;
+            std::cout << "--------------------------------------------------------------" << std::endl;
+        }
+
+    } catch (TCLAP::ArgException &e) {
+        std::cerr << "error: " << e.error() <<
+                     " for arg " << e.argId() << std::endl;
+        return -1;
+    }
 
     return 0;
 }
